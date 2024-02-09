8f17e33c9b2f49fdcc49cf84a514616aa1037d04
Nazifa16 <nazifa.abdullayeva@gmail.com>
2024-02-09 19:28:12 +0400

    When click on the categories, the data that will appear on the slider has been written to local storage

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,3 +1,7 @@
 "use strict";
 
 console.log("Search js");
+
+const searchInput = document.querySelector("#search-input");
+const searchBtn = document.querySelector("#search-btn");
+const carouselInner = document.querySelector("#carousel-inner");
